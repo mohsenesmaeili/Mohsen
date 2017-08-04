725ec05b9a4e222da4fa285f6901522d248df374
Mohsen Esmaeili <30568354+mohsenesmaeili@users.noreply.github.com>
2017-08-04 19:02:38 +0430

    Update XMLElement.lua

--- libs/XMLElement.lua
+++ libs/XMLElement.lua
@@ -1,6 +1,7 @@
 -- Copyright 2009 Leo Ponomarev. Distributed under the BSD Licence. 
 -- updated for module-free world of lua 5.3 on April 2 2015
 -- Not documented at all, but not interesting enough to warrant documentation anyway.
+
 local setmetatable, pairs, ipairs, type, getmetatable, tostring, error = setmetatable, pairs, ipairs, type, getmetatable, tostring, error
 local table, string = table, string
 
