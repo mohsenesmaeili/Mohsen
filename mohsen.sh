0c9e898c4a48a2c4b395d458c37a57ca89c5ecb2
Mohsen Esmaeili <30568354+mohsenesmaeili@users.noreply.github.com>
2017-08-04 18:42:23 +0430

    Update

--- mohsen.sh
+++ mohsen.sh
@@ -25,20 +25,20 @@ install() {
 }
 
 function print_logo() {
-	green "          ____  ____     _____"
-	green "         |  _ )|  _ \   |_   _|___ ____   __  __"
-	green "         |  _ \| |_) )    | |/ .__|  _ \_|  \/  |"
-	green "         |____/|____/     |_|\____/\_____|_/\/\_|"
+	green "                 "
+	green "          __  __ "
+	green "         |  \/  |"
+	green "         |_/\/\_|"
 	echo -e "\n\e[0m"
 }
 
 function logo_play() {
     declare -A txtlogo
     seconds="0.010"
-    txtlogo[1]=" ____  ____     _____"
-    txtlogo[2]="|  _ )|  _ \   |_   _|___ ____   __  __"
-    txtlogo[3]="|  _ \| |_) )    | |/ .__|  _ \_|  \/  |"
-    txtlogo[4]="|____/|____/     |_|\____/\_____|_/\/\_|"
+    txtlogo[1]=" "
+    txtlogo[2]=" __  __"
+    txtlogo[3]="|  \/  |"
+    txtlogo[4]="|_/\/\_|"
     printf "\e[31m\t"
     for i in ${!txtlogo[@]}; do
         for x in `seq 0 ${#txtlogo[$i]}`; do
@@ -51,14 +51,15 @@ function logo_play() {
 	echo -e "\e[0m"
 }
 
-function beyondteam() {
+
+function Mohsen() {
 	echo -e "\e[0m"
 	green "     >>>>                       We Are Not Attacker                             "
 	green "     >>>>                       We Are Not Alliance                             "
 	white "     >>>>                       We Are Programmer                               "
 	white "     >>>>                       We Are The Best                                 "
 	red   "     >>>>                       We Are Family                                   "
-	red   "     >>>>                       @BeyondTeam                                     "
+	red   "     >>>>                       @MohsenEsmaeili                                     "
 	echo -e "\e[0m"
 }
 
@@ -77,7 +78,7 @@ update() {
 
 if [ "$1" = "install" ]; then
 	print_logo
-	beyondteam
+	Mohsen
 	logo_play
 	install
   else
@@ -87,7 +88,7 @@ if [ ! -f ./tg/tgcli ]; then
     exit 1
  fi
 	print_logo
-	beyondteam
+	Mohsen
 	logo_play
    #sudo service redis-server restart
    ./tg/tgcli -s ./bot/bot.lua -l 1 -E $@
